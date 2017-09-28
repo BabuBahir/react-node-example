142b259fd83bd1cf51bc8bf42ef610d20cb13fe6
rohit <indians4german@gmail.com>
2017-09-28 17:36:26 +0530

    tic tac toe

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,30 +4,61 @@ import PropTypes from 'prop-types';
 import 'src/assets/stylesheets/base.scss';
 
 
-class App extends React.Component {
-
-   constructor(props) {
-      super(props);
-		
-      this.state = {
-         data: 'Initial data...'
-      }
-
-      this.updateState = this.updateState.bind(this);
-
-   };
-
-   updateState() {
-      this.setState({data: 'Data updated...'})
-   }
-
-   render() {
+class Square extends React.Component {
+    render() {
       return (
-         <div>
-            <button onClick = {this.updateState}>CLICK</button>
-            <h4>{this.state.data}</h4>
-         </div>
+        <button className="square">
+          {/* TODO */}
+        </button>
       );
-   }
-}
-export default App;
+    }
+  }
+  
+  class Board extends React.Component {
+    renderSquare(i) {
+      return <Square />;
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+  
+  class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    }
+  }
+  
+export default Game;
